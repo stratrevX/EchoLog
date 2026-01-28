c96e0e3c558c5c6de6b498508651d4319d35edaf
stratrevX <stratrevx@proton.me>
2026-01-28 13:24:07 +0100

    updated: \src\echolog\__init__.py

--- src/echolog/__init__.py
+++ src/echolog/__init__.py
@@ -1,6 +1,6 @@
 import sys
 if sys.version_info < (3, 9):
-    raise RuntimeError("EchoLog requires Python 3.10 or higher.")
+    raise RuntimeError("EchoLog requires Python 3.9 or higher.")
 
 from .logs import (
     infoLog,
